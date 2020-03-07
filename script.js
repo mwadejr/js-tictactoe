2d6207852f954e2328da57ff76972eecb79f6fd2
Michael Wade Jr <mwadejr7@gmail.com>
2020-03-07 12:24:05 -0500

    X & O functionality and appearance changes

---
+++ script.js
@@ -0,0 +1,44 @@
+var board
+const human = "O";
+const ai = "X";
+var track = human;
+const win = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+//startGame = () => {
+function startGame() {
+    document.querySelector(".endgame").getElementsByClassName.display = "none";
+    board = Array.from(Array(9).keys());
+    //console.log (board);
+    //console.log (cells);
+    for(let i=0; i<cells.length;i++){
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);  
+    }
+}
+
+function turnClick(square) {
+//console.log(square.target.id);
+turn(square.target.id, track)
+}
+
+function turn(squareID, player){
+board[squareID] = player;
+document.getElementById(squareID).innerText = player;
+if(track === human)
+track = ai;
+else
+track = human;
+}
